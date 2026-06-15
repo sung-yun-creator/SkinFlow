7bd23e6b8771509ecfed343983d340f9196d6300
kachoret77-prog <kachoret77@gmail.com>
2026-06-15 11:31:05 +0900

    feat: 제품 추천 API 및 올리브영 상품 seed 추가

--- backend/src/routes/recommendationRoutes.js
+++ backend/src/routes/recommendationRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const { listIngredientRecommendations } = require('../controllers/recommendationController');
+const {
+    listIngredientRecommendations,
+    listProductRecommendations,
+} = require('../controllers/recommendationController');
 const asyncHandler = require('../middlewares/asyncHandler');
 const { authenticate } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.get('/ingredients', authenticate, asyncHandler(listIngredientRecommendations));
+router.get('/products', authenticate, asyncHandler(listProductRecommendations));
 
 module.exports = router;
