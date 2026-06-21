82c52776b0837b7e85005919a4d8f021f3f0ebab
hyeongyeong <jhyeongyeong720@gmail.com>
2026-06-21 16:32:00 +0900

    fix chatbot integration issues

--- frontend/src/api/chatApi.js
+++ frontend/src/api/chatApi.js
@@ -1,4 +1,15 @@
-import axios from "axios";
+import http from "./http";
+
+async function sendChatMessage(message, analysisResult = null) {
+  return http.post("/api/chat", {
+    message,
+    analysisResult,
+  });
+}
+
+export {
+  sendChatMessage,
+};
 
 export const sendChatMessage = async (message) => {
   const response = await axios.post("http://localhost:3000/api/chat", {
